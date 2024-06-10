b081e9222ff9abb5ab27ca0da0b4d0a514d3ba4e
aein <mananniaein1234@gmail.com>
2024-06-10 21:23:58 +0330

    update new Version

--- patisa/src/components/latest-products/components/translate-box-card/index.tsx
+++ patisa/src/components/latest-products/components/translate-box-card/index.tsx
@@ -1,10 +1,17 @@
+import magnifire from "../icons/magnifier.svg";
+import love from "../icons/love-icon.svg";
+
 const TranslateBoxCarts = () => {
   return (
     <>
-      <div className="absolute bg-white  px-2 py-1 translate-y-12 bottom-2  duration-500 flex right-[33%] group-hover:translate-y-0 rounded-lg">
-        {Array.from(Array(2)).map((_t, index) => (
-          <div className={`w-6 h-6 ${index === 0 && "border-l-2"}`}></div>
-        ))}
+      <div className="absolute  bg-white  px-2 py-1 translate-y-12 bottom-2  duration-500 flex   right-[33%] group-hover:translate-y-0 rounded-sm">
+        <div className="bg-white flex gap-2">
+          {Array.from(Array(2)).map((_t, index) => (
+            <div className={` ${index === 0 && "border-l-2 pl-2"}`}>
+              {index === 0 ? <img src={magnifire} /> : <img src={love} />}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
