17aeb437fbf863e9d9c750ff55c137db5e81a989
aein <mananniaein1234@gmail.com>
2024-07-01 05:28:34 +0330

    update

--- patisa/src/components/patisa-footer/components/e-commerce-symbol/index.tsx
+++ patisa/src/components/patisa-footer/components/e-commerce-symbol/index.tsx
@@ -11,7 +11,7 @@ const E_CommerceSymbol = ({ children, parvanehImage, symbolImage }: Props) => {
     <CustomDiv className="lg:col-span-2 ">
       <div className="flex flex-col gap-4 h-full w-full  md:mt-0 border">
         {children}
-        <div className="flex md:gap-5 w-full">
+        <div className="flex gap-5 w-full">
           <img className="w-48 h-32" src={parvanehImage} />
           <img className="lg:w-36 lg:h-36" src={symbolImage} />
         </div>
